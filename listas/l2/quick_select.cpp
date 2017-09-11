0d54dc2b276092c41aadfd0d157a96e1a9175767
Vitor Barbosa <vitornga15@gmail.com>
2017-09-11 18:21:44 -0300

    Use quick_select in quick_sort

--- listas/l2/quick_select.cpp
+++ listas/l2/quick_select.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int get_median_out_of_5(vector<int> v){
     // use insertion sort of array of 5 elements
-    for(int i=1;i<v.size();i++){
+    for(int i=1;i < (int)v.size();i++){
         int j = i;
 
         while(j && (v[j] < v[j-1])){
@@ -20,10 +20,10 @@ int get_median_out_of_5(vector<int> v){
 int get_mom(vector<int> v){
     vector<int> medians;
 
-    for(int i=0;i<v.size();i+=5){
+    for(int i=0;i<(int)v.size();i+=5){
         // divide into sets of 5
         vector<int> group;
-        for(int j=i;j<i+5 && j < v.size();j++){
+        for(int j=i;j < i+5 && j < (int)v.size();j++){
             group.push_back(v[j]);
         }
 
@@ -40,6 +40,9 @@ int get_mom(vector<int> v){
 // mom = median of medians
 int k_smallest(vector<int> v, int k){
     int mom = get_mom(v);
+
+    if(v.size() <= 1) return mom;
+
     vector<int> left_values, right_values;
 
     for(auto c : v){
@@ -57,30 +60,40 @@ int k_smallest(vector<int> v, int k){
     return mom;
 }
 
-int main(){
-    int n;
-    vector<int> v;
+int k_smallest(vector<int> v, int k, int low, int high){
+    vector<int> temp;
 
-    cout << "Enter number of elements: ";
-    cin >> n;
-    cout << endl;
-
-    cout << "Enter elements: ";
-    cout << endl;
-
-    for(int i=0;i<n;i++){
-        int num;
-        cin >> num;
-        v.push_back(num);
+    for(int i=low;i<=high;i++){
+      temp.push_back(v[i]);
     }
 
-    int res =  k_smallest(v, (v.size() + 1) / 2);
-
-    cout << endl << "array: ";
-    for(auto c : v) cout << c << " ";
-    cout << endl << endl;
-
-    cout << "Median of array: " << res << endl;
-
-    return 0;
+    return k_smallest(temp, k);
 }
+//
+// int main(){
+//     int n;
+//     vector<int> v;
+//
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     cout << endl;
+//
+//     cout << "Enter elements: ";
+//     cout << endl;
+//
+//     for(int i=0;i<n;i++){
+//         int num;
+//         cin >> num;
+//         v.push_back(num);
+//     }
+//
+//     int res =  k_smallest(v, (v.size() + 1) / 2);
+//
+//     cout << endl << "array: ";
+//     for(auto c : v) cout << c << " ";
+//     cout << endl << endl;
+//
+//     cout << "Median of array: " << res << endl;
+//
+//     return 0;
+// }
--- listas/l2/quick_sort.cpp
+++ listas/l2/quick_sort.cpp
@@ -1,18 +1,25 @@
+#include "quick_select.cpp"
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int get_pivot_idx(int low, int high){
-    return (low + high) / 2;
-}
+// escolha do pivô como meio do vetor
+// int get_pivot(vector<int> v, int low, int high){
+//     return v[(low + high) / 2];
+// }
 
 void quick_sort(vector<int>& v, int low, int high){
-    cout << "low: " << low << " high: " << high << endl;
     if(low <= high){
         int i = low;
         int j = high;
-        int pivot = v[get_pivot_idx(low, high)];
-        
+
+        // escolha do pivô como meio do vetor
+        // int pivot = get_pivot(v, low, high);
+
+        // escolha do vetor utilizando quick_select e mediana das medianas
+        int pivot = k_smallest(v, (v.size() + 1) / 2, low, high);
+
         while(i <= j){
             while(v[i] < pivot) i++;
             while(pivot < v[j]) j--;
@@ -30,7 +37,7 @@ void quick_sort(vector<int>& v, int low, int high){
 }
 
 int main(){
-    vector<int> v { 1,5,2,8,3,7,4,6 };
+    vector<int> v { 1, 5, 2, 8, 3, 7, 4, 6 };
 
     cout << "Vetor inicial: ";
     for(auto c : v) cout << c << " "; cout << endl;
